b98d9718aeffba6d4c3b1b05618787303bfca4c7
yasasvi <yasasvigumma@gmail.com>
2026-03-09 13:22:19 +0530

    Added a server structure
    this will help use build the server.

--- modules/ai-server/action.go
+++ modules/ai-server/action.go
@@ -1,1 +1,11 @@
 package aiserver
+
+import "ai-server/modules/types"
+
+type AIPrompt types.AIPrompt
+
+func (ai *AIPrompt) Motivation() {}
+
+func (ai *AIPrompt) Analyse() {}
+
+func (ai *AIPrompt) Custom() {}
